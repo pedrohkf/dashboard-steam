d136729d3c30ca27929a5dde2cdd0306304bef01
pedrohkf <phkfaria@gmail.com>
2026-09-14 14:21:17 -0300

    fix: arrumando formatacao errada

--- fetch.js
+++ fetch.js
@@ -1,5 +1,4 @@
 fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=6D8689F868831603A8AE211A54E3A180&steamid=76561199387600663&format=json")
     .then(response => response.json())
     .then(data => console.log(data))
-    .catch(console.error(error => console.error(error));
-    )
+    .catch(error => console.error(error))
