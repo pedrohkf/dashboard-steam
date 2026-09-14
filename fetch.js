94b58f0bafaf7dc566b711a12c3cbbec506746bd
pedrohkf <phkfaria@gmail.com>
2026-09-14 14:55:27 -0300

    consumindo api da steam

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=22548B3C29EA4601ED7793C97E1CBCDF&steamid=76561199387600663&format=json")
+fetch("https://steam-api-swart.vercel.app/api/games?steamid=76561199387600663")
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error))
