06619ef48199b713c974ff40d3657b386f461223
pedrohkf <phkfaria@gmail.com>
2026-09-14 15:35:52 -0300

    consumindo biblioteca de jogos

--- fetch.js
+++ fetch.js
@@ -1,4 +1,26 @@
-fetch("https://steam-api-swart.vercel.app/api/games?steamid=76561199387600663")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
+const nameElement = document.querySelector(".name");
+const container = document.querySelector(".games");
+
+async function fetchGameData() {
+    try {
+        const response = await fetch("http://localhost:3000/api/games?steamid=76561199387600663");
+        const data = await response.json();
+
+        console.log(data);
+
+        nameElement.textContent = data.name;
+        data.games.forEach((game, indice) => {
+            const h1 = document.createElement("p")
+            const img = document.createElement("img")
+            h1.textContent = game.name
+            img.src = game.banner_url
+            container.append(h1)
+            container.append(img)
+        }
+        )
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+fetchGameData();
