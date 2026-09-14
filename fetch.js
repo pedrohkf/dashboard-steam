2baac5d011bb669b8231c949678f24116ce6203f
pedrohkf <phkfaria@gmail.com>
2026-09-14 14:27:14 -0300

    mudando key

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=6D8689F868831603A8AE211A54E3A180&steamid=76561199387600663&format=json")
+fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=22548B3C29EA4601ED7793C97E1CBCDF&steamid=76561199387600663&format=json")
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error))
